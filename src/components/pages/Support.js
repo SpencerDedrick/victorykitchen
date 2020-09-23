e1acbdc5686e4d26a1b4ae1b022da51f02cf485a
Spencer D <spencer.dedrick@gmail.com>
2020-09-23 13:05:39 -0500

    added the support links and buttons fixed some ui issues

--- src/components/pages/Support.js
+++ src/components/pages/Support.js
@@ -1,11 +1,41 @@
 import React from "react";
+import { Button } from "../Button";
 
 import "../../App.css";
 
 export default function Support() {
   return (
     <>
-      <h1 className="support">SUPPORT</h1>
+      <div className="support">
+        SUPPORT OUR WORK
+        <div className="hero-btns">
+          <a
+            className="support--btn"
+            buttonStyle="btn--Primary"
+            buttonSize="btn--large"
+            href="https://www.paypal.com/paypalme/victorykitchen"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Donate
+          </a>
+          <a
+            className="support--btn"
+            href="https://www.ofnoreputation.com/collections/causes/products/victory-kitchen"
+            rel="noopener noreferrer"
+          >
+            Shop
+          </a>
+          <Button
+            className="support--btn"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+            path="volunteer"
+          >
+            Volunteer
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/pages/Volunteer.js
+++ src/components/pages/Volunteer.js
@@ -5,15 +5,12 @@ import "../../App.css";
 
 export default function Volunteer() {
   return (
-    <>
-      <h1 className="volunteer">VOLUNTEER</h1>;
+    <div className="volunteer_wrapper">
+      <div className="volunteer">
+        VOLUNTEER<h4>See below for our upcoming volunteer opportunities.</h4>
+      </div>
+      ;
       <div className="volunteer_card_wrapper">
-        <CardItem
-          src={require("../../Assets/Photo15.jpeg")}
-          text=" You can still purchase a shirt from VK! So far we have raised $100+ from your purchases! You can also help me to raise awareness by taking a selfie in your VK shirt and send it to me by this Wednesday!"
-          label="Fundraiser!"
-          path="/"
-        />
         <CardItem
           src={require("../../Assets/Photo16.jpeg")}
           text="Serving 50+ lunch for those displaced by Hurricane Laura. 
@@ -21,7 +18,7 @@ export default function Volunteer() {
 -2-3 volunteers to help package/transport 
 -1 prayer partner"
           label="September 25th"
-          path="/"
+          path="/volunteer"
         />
         <CardItem
           src={require("../../Assets/photo11.jpg")}
@@ -29,7 +26,7 @@ export default function Volunteer() {
 -sort and package Bibles and toiletries for street ministries. (5-6 volunteers)
 -1 volunteer to pick up food items for street ministries"
           label="October 3"
-          path="/"
+          path="/volunteer"
         />
         <CardItem
           src={require("../../Assets/Photo14.jpeg")}
@@ -38,10 +35,10 @@ export default function Volunteer() {
 -(5 volunteers) Serve meals, interact with community, prayer as needed.
 -1-2 volunteers to help me cook/ or prepare a dessert"
           label="October 6"
-          path="/"
+          path="/volunteer"
         />
       </div>
       ;
-    </>
+    </div>
   );
 }
