a3678e62fc0320cff712ae98e313fd87569347e4
Spencer D <spencer.dedrick@gmail.com>
2020-10-23 19:05:59 -0500

    attempted to fix background scrolling bug

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -3,9 +3,9 @@ import "./Panel.css";
 
 function Panel(props) {
   return (
-    <div className={`panel ${props.class}`}>
+    <div className={`panel background-image ${props.class}`}>
       <div className={props.fade}></div>
-      <div className="panel-container">{props.children}</div>
+      {props.children}
     </div>
   );
 }
